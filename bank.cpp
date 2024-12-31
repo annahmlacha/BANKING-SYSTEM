5b68a12548c2ad066043801176d4f6d54316f5c8
mlacha <annahmlacha@gmail>
2024-12-31 13:34:22 +0300

    add constructor

--- bank.cpp
+++ bank.cpp
@@ -8,3 +8,11 @@ private:
     string accountHolder;
     int accountNumber;
     double balance;
+    
+public:
+    // Constructor to initialize the account with an account holder name, number, and initial balance
+    BankAccount(string name, int accountNum, double initialBalance) {
+        accountHolder = name;
+        accountNumber = accountNum;
+        balance = initialBalance;
+    }
